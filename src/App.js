d6ac64c5695823727d7c73e835aad565f20d9e44
siwoo <ejeeny@gmail.com>
2024-07-07 00:35:37 +0900

    update

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ import theme from './styles/theme';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home/Home';
-import HomeEn from './pages/Home/HomeEn';
 import Members from './pages/Members';
 import Donation from './pages/Donation/Donation';
-import DonationEn from './pages/Donation/DonationEn';
 import NotFound from './pages/NotFound';
 
 function RouterContainer() {
@@ -23,10 +21,8 @@ function RouterContainer() {
         <Box as="main" flex="1" py={8}>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/en" element={<HomeEn />} />
             <Route path="/members" element={<Members />} />
             <Route path="/donation" element={<Donation />} />
-            <Route path="/donation/en" element={<DonationEn />} />
             <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
           </Routes>
         </Box>
